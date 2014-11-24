07ee39e92547d1f36b5a9fbe9ec5e875e6e8db3e
Shawn Campbell <jaegerpicker@gmail.com>
2014-11-23 21:55:32 -0500

    setup go

--- elpa/go-mode-20141120.1935/go-mode-autoloads.el
+++ elpa/go-mode-20141120.1935/go-mode-autoloads.el
@@ -4,7 +4,7 @@
 
 
 ;;;### (autoloads (go-download-play godoc gofmt-before-save go-mode)
-;;;;;;  "go-mode" "go-mode.el" (21618 38564 0 0))
+;;;;;;  "go-mode" "go-mode.el" (21618 40088 0 0))
 ;;; Generated autoloads from go-mode.el
 
 (autoload 'go-mode "go-mode" "\
@@ -88,7 +88,7 @@ buffer. Tries to look for a URL at point.
 
 ;;;***
 
-;;;### (autoloads nil nil ("go-mode-pkg.el") (21618 38564 798352
+;;;### (autoloads nil nil ("go-mode-pkg.el") (21618 40088 664664
 ;;;;;;  0))
 
 ;;;***
